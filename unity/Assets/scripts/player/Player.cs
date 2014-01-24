b6b61da06bcaf36b65ae62475c9d8fbeeab65b10
Shane Marks <shane@renderheads.com>
2014-01-24 22:33:34 +0200

    basic scene setup

---
+++ unity/Assets/scripts/player/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player: MonoBehaviour {
+		
+	public bool IsAlive;
+	public string PlayerName;
+
+	public Movement _movement;
+	public Vector3 StartPos;
+	public Transform _trans;
+
+	public ItemType[] ItemsOwned;
+
+
+	void Start ()
+	{
+		_movement = GetComponent <Movement> ();
+		_trans = gameObject.transform;
+		_trans.localPosition = StartPos;
+	}
+
+	public void KillPlayer ()
+	{
+		Debug.Log ("Kill player");
+		IsAlive =  false;
+		_movement.enabled = false;
+	}
+	
+	void ResetPlayer ()
+	{
+		_movement.enabled = true;
+	}
+}
+
