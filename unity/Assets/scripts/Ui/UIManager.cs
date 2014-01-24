8c346973f634ac37f51e0083f9a25b6b8408969e
Shane Marks <shane@renderheads.com>
2014-01-24 22:46:59 +0200

    no message

---
+++ unity/Assets/scripts/Ui/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	void AnswerPhone ()
+	{
+		Debug.Log ("Answer Phone");
+	}
+
+	void  StartGame ()
+	{
+		Debug.Log ("Start Game");
+	}
+}
