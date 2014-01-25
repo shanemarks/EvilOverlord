c0a10ad8ef8fe9f241fa854242f647c44cda9c06
Marc Burgess <mburgessza@gmail.com>
2014-01-25 15:35:22 +0200

    Merging more

--- unity/Assets/scripts/player/Movement.cs
+++ unity/Assets/scripts/player/Movement.cs
@@ -151,15 +151,9 @@ public class Movement : MonoBehaviour {
 	{
 
 		Boundary b = CheckHitGetBoundary(Vector3.down + 2*Vector3.right);
-
 		
-<<<<<<< HEAD
 		if (!CheckHit(Vector3.down + 2*Vector3.right )) _trans.localPosition -=  new Vector3 (-2,1, 0).normalized * MovementSpeed ;
 		else 
-=======
-		if (!CheckHit(Vector3.down)) _trans.localPosition += new Vector3 (0,-MovementSpeed, 0);
-	/*	else 
->>>>>>> 4bd98dc489771a3aa73bf657e2dcdd33f3a0e414
 		{
 			if (b != null)
 			{
@@ -168,7 +162,7 @@ public class Movement : MonoBehaviour {
 			}
 
 			else  if (!CheckHit(Vector3.right))   _trans.localPosition += new Vector3 (MovementSpeed,0, 0);
-		}*/
+		}
 	}
 
 	void MoveLeft ()
@@ -195,6 +189,7 @@ public class Movement : MonoBehaviour {
 				
 			}
 
+			UIManager.instance.CreateObjectPickupAnimation (gameObject.transform.position,"Object Picked Up");
 			GameController.instance.PlayerActivatedLocation (_thePlayer, _thePlayer.OnRoomLocation.roomObjectType);
 
 
