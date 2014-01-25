be898ad1ec096f7285fcf44bcf6092260cb968eb
Shane Marks <shane@renderheads.com>
2014-01-25 02:13:25 +0200

    no message

---
+++ unity/Assets/scripts/Boundary.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boundary : MonoBehaviour {
+	public enum Direction {
+	Up,
+	Down,
+	Left, 
+	Right
+}
+
+	public Direction PreferredSlideDirection = Direction.Left;
+}
