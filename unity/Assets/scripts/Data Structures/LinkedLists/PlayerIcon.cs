0fb81776d13fc049c80ce94a5e029581404642f2
Shane Marks <info@shanemarks.co.za>
2014-01-30 08:01:45 +0200

    Forgot to commit PlayerIcon UIScript

---
+++ unity/Assets/scripts/Data Structures/LinkedLists/PlayerIcon.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerIcon : LinkBase {
+	void Awake ()
+	{
+		CalculateLinkPosition ();
+	}
+
+	public UISprite Border;
+	public UILabel  Points;
+	public UISprite Icon;
+}
