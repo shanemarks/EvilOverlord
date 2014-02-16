300002511de3ab42983f781fe6d8fb97388b94cd
Shane Marks <shane@renderheads.com>
2014-02-16 11:37:44 +0200

    forgot to commit these.

---
+++ unity/Assets/scripts/animation/Pulse.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulse : MonoBehaviour {
+
+	public UISprite _sprite;
+	Color c = Color.white;
+	
+	float counter = 0;
+	public float speed = 2 ;
+
+	void Update ()
+	{
+		c = new Color (1,1,1, Mathf.Abs (Mathf.Sin (counter) ) );
+		counter += Time.deltaTime*speed;
+		_sprite.color = c;
+
+	}
+
+
+}
